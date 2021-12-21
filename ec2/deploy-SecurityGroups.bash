874f363346f3da8cda206309b42a6205ed8ab317
Michael Soh <2767724+sohmc@users.noreply.github.com>
2021-12-21 17:04:42 -0500

    All Traffic for Galactica

--- ec2/deploy-SecurityGroups.bash
+++ ec2/deploy-SecurityGroups.bash
@@ -2,7 +2,7 @@
 
 set -ev
 
-aws cloudformation create-stack --stack-name SecurityGroup-Deploy --template-body file://default-security-groups.yaml \
+aws cloudformation update-stack --stack-name SecurityGroup-Deploy --template-body file://default-security-groups.yaml \
   --parameters ParameterKey=deployDate,ParameterValue=$(date -Isec) \
                ParameterKey=commitSha,ParameterValue=$(git rev-parse HEAD) \
                ParameterKey=galacticaIp,ParameterValue=$(curl -s http://checkip.amazonaws.com/)/32
