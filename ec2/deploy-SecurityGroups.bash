76fc62bf10819c00460de18a35b03a66f0db3c4c
Michael Soh <2767724+sohmc@users.noreply.github.com>
2021-12-22 14:33:28 -0500

    Removed bespoke script

--- ec2/deploy-SecurityGroups.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ev
-
-aws cloudformation update-stack --stack-name SecurityGroup-Deploy --template-body file://default-security-groups.yaml \
-  --parameters ParameterKey=deployDate,ParameterValue=$(date -Isec) \
-               ParameterKey=commitSha,ParameterValue=$(git rev-parse HEAD)
